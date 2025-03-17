a7daac27a60cf2bfbd25f365ccb9a20facc50ca4
Adam-Hrzeta <112137532+Adam-Hrzeta@users.noreply.github.com>
2025-03-17 13:53:49 -0600

    perfil

--- app.py
+++ app.py
@@ -14,10 +14,6 @@ def login():
 def register():
     return render_template('register.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
-
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
